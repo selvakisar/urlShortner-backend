5aeb0fcc56779c225e96b1345f9dc5e981436689
selvakisar <130025654+selvakisar@users.noreply.github.com>
2023-11-24 21:13:14 +0530

    modified

--- routes/url.js
+++ routes/url.js
@@ -25,7 +25,7 @@ router.get("/all",async (req, res) => {
 
 router.post("/short",async (req, res) => {
     console.log("HERE",req.body.url);
-    const { origUrl } = req.body;
+    const  origUrl  = req.body;
     const base = `https://nanourl.onrender.com/url`;
   
     const urlId = shortid.generate();
